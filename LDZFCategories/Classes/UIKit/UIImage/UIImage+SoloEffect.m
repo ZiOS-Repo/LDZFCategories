3e01c76f54d6d7dd888c976658449e74caafcae6
yt_zhuyuhui <C2021900@pccc.bankcomm.com>
2021-12-14 15:41:30 +0800

    更新

--- LDZFCategories/Classes/UIKit/UIImage/UIImage+SoloEffect.m
+++ LDZFCategories/Classes/UIKit/UIImage/UIImage+SoloEffect.m
@@ -33,86 +33,74 @@
     return blurImage;
 }
 
-- (UIImage *)solo_blurImageWithAccelerateBlurValue:(CGFloat)blurValue
-{
-    if (self == nil) { return nil; }
-    //模糊度,
-    if (blurValue < 0.0f) {
-        blurValue = 0.1f;
-    } else if (blurValue > 1.0f) {
-        blurValue = 1.0f;
+- (UIImage *)solo_blurImageWithAccelerateBlurValue:(CGFloat)blurValue {
+    if (self == nil) {
+        return nil;
     }
-    //boxSize必须大于0
-    int boxSize = (int)(blurValue * 100);
-    boxSize -= (boxSize % 2) + 1;
-    //图像处理
+    if (blurValue < 0.f || blurValue > 1.f) {
+        blurValue = 0.5f;
+    }
+    int boxSize = (int)(blurValue * 40);
+    boxSize = boxSize - (boxSize % 2) + 1;
+    
     CGImageRef img = self.CGImage;
     
-    //需要引入#import <Accelerate/Accelerate.h>
-    //图像缓存,输入缓存，输出缓存
     vImage_Buffer inBuffer, outBuffer;
     vImage_Error error;
-    //像素缓存
-    void *pixelBuffer;
-
-    //数据源提供者，Defines an opaque type that supplies Quartz with data.
-    CGDataProviderRef inProvider = CGImageGetDataProvider(img);
-    // provider’s data.
-    CFDataRef inBitmapData = CGDataProviderCopyData(inProvider);
     
-    //宽，高，字节/行，data
+    void *pixelBuffer;
+    //从CGImage中获取数据
+    CGDataProviderRef inProvider = CGImageGetDataProvider(img);
+    CFDataRef inBitmapData = CGDataProviderCopyData(inProvider);
+    //设置从CGImage获取对象的属性
     inBuffer.width = CGImageGetWidth(img);
     inBuffer.height = CGImageGetHeight(img);
     inBuffer.rowBytes = CGImageGetBytesPerRow(img);
+    
     inBuffer.data = (void*)CFDataGetBytePtr(inBitmapData);
-
-    //像数缓存，字节行*图片高
-    pixelBuffer = malloc(CGImageGetBytesPerRow(img) * CGImageGetHeight(img));
+    
+    pixelBuffer = malloc(CGImageGetBytesPerRow(img) *
+                         CGImageGetHeight(img));
+    
+    if(pixelBuffer == NULL)
+        NSLog(@"No pixelbuffer");
+    
     outBuffer.data = pixelBuffer;
     outBuffer.width = CGImageGetWidth(img);
     outBuffer.height = CGImageGetHeight(img);
     outBuffer.rowBytes = CGImageGetBytesPerRow(img);
-    // 第三个中间的缓存区,抗锯齿的效果
-    void *pixelBuffer2 = malloc(CGImageGetBytesPerRow(img) * CGImageGetHeight(img));
-    vImage_Buffer outBuffer2;
-    outBuffer2.data = pixelBuffer2;
-    outBuffer2.width = CGImageGetWidth(img);
-    outBuffer2.height = CGImageGetHeight(img);
-    outBuffer2.rowBytes = CGImageGetBytesPerRow(img);
-    //Convolves a region of interest within an ARGB8888 source image by an implicit M x N kernel that has the effect of a box filter.
-    error = vImageBoxConvolve_ARGB8888(&inBuffer, &outBuffer2, NULL, 0, 0, boxSize, boxSize, NULL, kvImageEdgeExtend);
-    error = vImageBoxConvolve_ARGB8888(&outBuffer2, &inBuffer, NULL, 0, 0, boxSize, boxSize, NULL, kvImageEdgeExtend);
-    error = vImageBoxConvolve_ARGB8888(&inBuffer, &outBuffer, NULL, 0, 0, boxSize, boxSize, NULL, kvImageEdgeExtend);
-    if (error) {
-       NSLog(@"error from convolution %ld", error);
-    }
-    //    NSLog(@"字节组成部分：%zu",CGImageGetBitsPerComponent(img));
-    //颜色空间DeviceRGB
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
     
-    //用图片创建上下文,CGImageGetBitsPerComponent(img),7,8
+    error = vImageBoxConvolve_ARGB8888(&inBuffer, &outBuffer, NULL, 0, 0, boxSize, boxSize, NULL, kvImageEdgeExtend);
+    
+    if (error) {
+        NSLog(@"error from convolution %ld", error);
+    }
+    
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
     CGContextRef ctx = CGBitmapContextCreate(
-                                            outBuffer.data,
-                                            outBuffer.width,
-                                            outBuffer.height,
-                                            8,
-                                            outBuffer.rowBytes,
-                                            colorSpace,
-                                            CGImageGetBitmapInfo(self.CGImage));
-
-    //根据上下文，处理过的图片，重新组件
+                                             outBuffer.data,
+                                             outBuffer.width,
+                                             outBuffer.height,
+                                             8,
+                                             outBuffer.rowBytes,
+                                             colorSpace,
+                                             kCGImageAlphaNoneSkipLast);
     CGImageRef imageRef = CGBitmapContextCreateImage (ctx);
     UIImage *returnImage = [UIImage imageWithCGImage:imageRef];
+    
     //clean up
     CGContextRelease(ctx);
     CGColorSpaceRelease(colorSpace);
+    
     free(pixelBuffer);
-    free(pixelBuffer2);
     CFRelease(inBitmapData);
-    //CGColorSpaceRelease(colorSpace);   //多余的释放
+    
+    CGColorSpaceRelease(colorSpace);
     CGImageRelease(imageRef);
+    
     return returnImage;
 }
+
 
 -(UIImage *)solo_applyLightEffect
 {
